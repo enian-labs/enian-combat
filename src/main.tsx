6ff615004b4b76190224b9af6d7a9e4632cfc227
Dwi Yulianto <82428562+docd-dev@users.noreply.github.com>
2024-10-25 21:05:48 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,28 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { RouterProvider, createRouter } from "@tanstack/react-router";
-import { routeTree } from "./routeTree.gen";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createRouter } from '@tanstack/react-router';
+import { routeTree } from './routeTree.gen';
 
 // import tailwind css
-import "./styles/index.css";
+import './styles/index.css';
 
 // Set up a Router instance
 const router = createRouter({
-  routeTree,
-  defaultPreload: "intent",
+   basepath: '/enian-combat.github.io',
+   routeTree,
+   defaultPreload: 'intent',
 });
 
 // Register things for typesafety
-declare module "@tanstack/react-router" {
-  interface Register {
-    router: typeof router;
-  }
+declare module '@tanstack/react-router' {
+   interface Register {
+      router: typeof router;
+   }
 }
 
-const rootElement = document.getElementById("app")!;
+const rootElement = document.getElementById('app')!;
 
 if (!rootElement.innerHTML) {
-  const root = ReactDOM.createRoot(rootElement);
-  root.render(<RouterProvider router={router} />);
+   const root = ReactDOM.createRoot(rootElement);
+   root.render(<RouterProvider router={router} />);
 }
